868bf60534cad5148496ff62cfe47a17a8e54d73
marc0x71 <marc0x71@yahoo.it>
2024-10-05 15:10:48 +0200

    Configurazione base

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,17 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function () 
+    local configs = require("nvim-treesitter.configs")
+
+    configs.setup({
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },  
+    })
+    vim.wo.foldmethod = 'expr'
+    vim.wo.foldlevel=99
+    vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+  end
+}
