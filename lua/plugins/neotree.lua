1ef3e30abc53f48f84d07ab1ccd4705a0da187a6
Marco <marc0x71@yahoo.it>
2024-11-14 23:00:28 +0100

    NeoTree

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,32 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  keys = {
+    { "\\\\", "<cmd>Neotree toggle<cr>", desc="Show neotree"},
+  },
+  config=function ()
+    require("neo-tree").setup({
+      close_if_last_window = false,
+      event_handlers = {
+
+        {
+          event = "file_open_requested",
+          handler = function()
+            -- auto close
+            -- vim.cmd("Neotree close")
+            -- OR
+            require("neo-tree.command").execute({ action = "close" })
+          end
+        },
+
+      }
+
+    })
+  end
+}
