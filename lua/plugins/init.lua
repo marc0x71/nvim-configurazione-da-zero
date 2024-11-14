4d8e38d2f328e903312e18f923cf7f98d6ea3b94
Marco <marc0x71@yahoo.it>
2024-11-14 23:02:28 +0100

    Aggiunti alcuni plugin utili Corretta formattazione

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,75 +1,78 @@
 return {
-  "folke/which-key.nvim",
-  { "folke/neoconf.nvim", cmd = "Neoconf" },
-  {
-    "folke/lazydev.nvim",
-    ft = "lua", -- only load on lua files
-    opts = {
-      library = {
-        -- See the configuration section for more details
-        -- Load luvit types when the `vim.uv` word is found
-        { path = "luvit-meta/library", words = { "vim%.uv" } },
-      },
-    },
-  },
-  { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
-  { -- optional completion source for require statements and module annotations
-    "hrsh7th/nvim-cmp",
-    opts = function(_, opts)
-      opts.sources = opts.sources or {}
-      table.insert(opts.sources, {
-        name = "lazydev",
-        group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-      })
-    end,
-  },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000,
-    config = function() 
-      vim.cmd.colorscheme "catppuccin"
-    end
-  },
-  {
-    "folke/trouble.nvim",
-    opts = {}, -- for default options, refer to the configuration section for custom setup.
-    cmd = "Trouble",
-    keys = {
-      {
-        "<leader>xx",
-        "<cmd>Trouble diagnostics toggle<cr>",
-        desc = "Diagnostics (Trouble)",
-      },
-      {
-        "<leader>xX",
-        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-        desc = "Buffer Diagnostics (Trouble)",
-      },
-      {
-        "<leader>cs",
-        "<cmd>Trouble symbols toggle focus=false<cr>",
-        desc = "Symbols (Trouble)",
-      },
-      {
-        "<leader>cl",
-        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-        desc = "LSP Definitions / references / ... (Trouble)",
-      },
-      {
-        "<leader>xL",
-        "<cmd>Trouble loclist toggle<cr>",
-        desc = "Location List (Trouble)",
-      },
-      {
-        "<leader>xQ",
-        "<cmd>Trouble qflist toggle<cr>",
-        desc = "Quickfix List (Trouble)",
-      },
-    },
-  },
-  {
-    'sbdchd/neoformat',
-    keys = {
-      { "<leader>cf", "<cmd>Neoformat<cr>", desc = "Code format" }
-    }
-  }
+    "folke/which-key.nvim", {"folke/neoconf.nvim", cmd = "Neoconf"}, {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+            library = {
+                -- See the configuration section for more details
+                -- Load luvit types when the `vim.uv` word is found
+                {path = "luvit-meta/library", words = {"vim%.uv"}}
+            }
+        }
+    }, {"Bilal2453/luvit-meta", lazy = true}, -- optional `vim.uv` typings
+    { -- optional completion source for require statements and module annotations
+        "hrsh7th/nvim-cmp",
+        opts = function(_, opts)
+            opts.sources = opts.sources or {}
+            table.insert(opts.sources, {
+                name = "lazydev",
+                group_index = 0 -- set group index to 0 to skip loading LuaLS completions
+            })
+        end
+    }, {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        init = function()
+            vim.cmd.colorscheme "catppuccin"
+            vim.cmd.hi 'Comment gui=none'
+        end,
+        opts = {transparent_background = true}
+    }, {
+        "folke/trouble.nvim",
+        opts = {}, -- for default options, refer to the configuration section for custom setup.
+        cmd = "Trouble",
+        keys = {
+            {
+                "<leader>xx",
+                "<cmd>Trouble diagnostics toggle<cr>",
+                desc = "Diagnostics (Trouble)"
+            }, {
+                "<leader>xX",
+                "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+                desc = "Buffer Diagnostics (Trouble)"
+            }, {
+                "<leader>cs",
+                "<cmd>Trouble symbols toggle focus=false<cr>",
+                desc = "Symbols (Trouble)"
+            }, {
+                "<leader>cl",
+                "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+                desc = "LSP Definitions / references / ... (Trouble)"
+            }, {
+                "<leader>xL",
+                "<cmd>Trouble loclist toggle<cr>",
+                desc = "Location List (Trouble)"
+            }, {
+                "<leader>xQ",
+                "<cmd>Trouble qflist toggle<cr>",
+                desc = "Quickfix List (Trouble)"
+            }
+        }
+    }, {
+        'sbdchd/neoformat',
+        keys = {{"<leader>cf", "<cmd>Neoformat<cr>", desc = "Code format"}}
+    }, {
+        'lewis6991/gitsigns.nvim',
+        config = function() require('gitsigns').setup() end
+    }, {'nanotee/luv-vimdocs'}, {'milisims/nvim-luaref'}, {
+        'linrongbin16/lsp-progress.nvim',
+        config = function() require('lsp-progress').setup() end
+    }, {
+        "j-hui/fidget.nvim",
+        config = function()
+            require("fidget").setup({notification = {window = {winblend = 0}}})
+        end
+    }, {"andrejlevkovitch/vim-lua-format"}, {"tpope/vim-fugitive"}
 }
 
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,10 +1,42 @@
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' } ,
-  config = function() 
-    local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-  end
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.8',
+        dependencies = {'nvim-lua/plenary.nvim'},
+        config = function()
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>ff', builtin.find_files,
+                           {desc = 'Telescope find files'})
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep,
+                           {desc = 'Telescope live grep'})
+            vim.keymap.set('n', '<leader><leader>', builtin.buffers,
+                           {desc = 'Telescope buffers'})
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags,
+                           {desc = 'Telescope help tags'})
+        end
+    }, {
+        "allaman/emoji.nvim",
+        version = "1.0.0", -- optionally pin to a tag
+        ft = "markdown", -- adjust to your needs
+        dependencies = {
+            -- util for handling paths
+            "nvim-lua/plenary.nvim", -- optional for nvim-cmp integration
+            "hrsh7th/nvim-cmp", -- optional for telescope integration
+            "nvim-telescope/telescope.nvim"
+        },
+        opts = {
+            -- default is false
+            enable_cmp_integration = true,
+            -- optional if your plugin installation directory
+            -- is not vim.fn.stdpath("data") .. "/lazy/
+            -- plugin_path = vim.fn.expand("$HOME/plugins/")
+        },
+        config = function(_, opts)
+            require("emoji").setup(opts)
+            -- optional for telescope integration
+            local ts = require('telescope').load_extension 'emoji'
+            vim.keymap.set('n', '<leader>fe', ts.emoji,
+                           {desc = 'Search [E]moji'})
+        end
+    }
 }
